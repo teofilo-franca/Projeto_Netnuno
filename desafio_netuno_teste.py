caaea9d991d4fb9e577d8b4c43ed262a24b17cae
teofilo franca <teo.aguia@gmail.com>
2024-02-18 19:51:32 -0300

    desafio finalizado

--- desafio_netuno_teste.py
+++ desafio_netuno_teste.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+import re
 
 class Avatar:
     def __init__(self, nome, data_nascimento, email, senha, confirmar_senha):
@@ -22,31 +23,57 @@ class Avatar:
         self.altura = altura
         self.forca = forca
 
-    def mostrar_informacoes_customizacao(self):
-        print("\n** Informações de customização **")
+    def mostrar_informacoes(self, personagem, ferramenta_batalha, montaria):
+        print(f"Nome: {self.nome}")
+        print(f"Email: {self.email}")
+        print(f"Personagem: {personagem['nome']}")
         print(f"Cabelo: {self.cabelo}")
         print(f"Pele: {self.pele}")
         print(f"Altura: {self.altura} cm")
         print(f"Força: {self.forca}")
+        print(f"Vida: {personagem['vida']}")
+        print(f"Mana: {personagem['mana']}")
+        print(f"Velocidade de Ataque: {personagem['velocidade_ataque']}")
+        print(f"Ferramenta de batalha: {ferramenta_batalha}")
+        print(f"Montaria: {montaria['nome']}")
+        print(f"Velocidade da montaria: {montaria['velocidade']}")
+        print(f"Tempo para descanso da montaria: {montaria['tempo_descanso']}")
+
+def validar_data_nascimento(data_nascimento):
+    datetime.strptime(data_nascimento, "%d/%m/%Y")
 
 def imprimir_letra_por_letra(texto):
     for letra in texto:
         print(letra, end='', flush=True)
-        time.sleep(0.1) 
-
-texto_para_imprimir = " * * * * * JOGO NETUNO * * * * * "
-imprimir_letra_por_letra(texto_para_imprimir)
-print()
-print()
-time.sleep(1)
-print("Há algo de bom neste mundo e vale a pena lutar por isso")
-print()
-time.sleep(1)
+        time.sleep(0.1)
+    print()
 
 def tela_cadastro():
-    nome_completo = input("Nome completo: ")
-    data_nascimento = input("Data de nascimento (dd/mm/aaaa): ")
-    email = input("Email: ")
+    imprimir_letra_por_letra(" * * * * * JOGO NETUNO * * * * * ")
+    print()
+    time.sleep(1)
+    print("Há algo de bom neste mundo e vale a pena lutar por isso")
+    print()
+    time.sleep(1)
+
+    while True:
+        nome_completo = input("Nome completo: ")
+        data_nascimento = input("Data de nascimento (dd/mm/aaaa): ")
+
+        try:
+            validar_data_nascimento(data_nascimento)
+            break
+        except ValueError:
+            print("Formato de data inválido. Tente novamente.")
+
+    while True:
+        email = input("Email: ")
+
+        if re.match(r"[^@]+@[^@]+\.[^@]+", email):  # Verifica o formato do email
+            break
+        else:
+            print("Formato de email inválido. Tente novamente.")
+
     senha = input("Senha: ")
     confirmar_senha = input("Confirmar senha: ")
 
@@ -72,84 +99,79 @@ def validar_idade(data_nascimento):
     return idade >= 18
 
 def tela_login(avatar):
-    print("\n*** LOGIN ***")
-    email = input("Email: ")
-    senha = input("Senha: ")
+    while True:
+        print("\n*** LOGIN ***")
+        email = input("Email: ")
+        senha = input("Senha: ")
 
-    if not avatar.validar_login(email, senha):
-        print("Credenciais incorretas.")
-        return None
-
-    print("\nLogin bem-sucedido!")
-    return avatar
+        if avatar.validar_login(email, senha):
+            print("\nLogin bem-sucedido!")
+            return avatar
+        else:
+            print("Credenciais incorretas. Tente novamente.")
 
 def tela_jogo(avatar):
     print("\n### TELA DO JOGO NETUNO ###")
-    classe = escolher_classe()
-    ferramenta_batalha = escolher_ferramenta_batalha(classe)
+    print()
+    personagem = escolher_personagem()
+    ferramenta_batalha = escolher_ferramenta_batalha(personagem)
     montaria = escolher_montaria()
 
     customizar_avatar(avatar)
 
-    distribuir_pontos_classe(classe)
-    distribuir_pontos_montaria(montaria)
+    avatar.mostrar_informacoes(personagem, ferramenta_batalha, montaria)
 
-    mostrar_informacoes(avatar, classe, ferramenta_batalha, montaria)
-    avatar.mostrar_informacoes_customizacao()
+    
+    print("\n       Obrigado por jogar o JOGO NETUNO!")
 
-def escolher_classe():
+def escolher_personagem():
+    personagens = [
+        {"nome": "Paladino", "vida": 70, "mana": 30, "velocidade_ataque": 1.80},
+        {"nome": "Atirador", "vida": 80, "mana": 20, "velocidade_ataque": 1.95},
+        {"nome": "Guerreiro", "vida": 90, "mana": 10, "velocidade_ataque": 2.00},
+        {"nome": "Bárbaro", "vida": 85, "mana": 15, "velocidade_ataque": 1.60},
+        {"nome": "Arqueiro", "vida": 75, "mana": 25, "velocidade_ataque": 2.20},
+    ]
+
     print("----- Escolha seu Herói -----")
-    print("[1] Paladino")
-    print("[2] Atirador")
-    print("[3] Guerreiro")
-    print("[4] Bárbaro")
-    print("[5] Arqueiro")
+    for i, personagem in enumerate(personagens, 1):
+        print(f"[{i}] {personagem['nome']}")
 
     while True:
-        class_choice = input("Escolha o número do seu Herói: ")
+        personagem_choice = input("Escolha o número do seu Herói: ")
 
-        if class_choice == "1":
-            print("Você escolheu o Paladino.")
-            return class_choice
-        elif class_choice == "2":
-            print("Você escolheu o Atirador.")
-            return class_choice
-        elif class_choice == "3":
-            print("Você escolheu o Guerreiro.")
-            return class_choice
-        elif class_choice == "4":
-            print("Você escolheu o Bárbaro.")
-            return class_choice
-        elif class_choice == "5":
-            print("Você escolheu o Arqueiro.")
-            return class_choice
+        if personagem_choice.isdigit() and 1 <= int(personagem_choice) <= len(personagens):
+            index = int(personagem_choice) - 1
+            personagem_escolhido = personagens[index]
+            print(f"Você escolheu o {personagem_escolhido['nome']}.")
+            return personagem_escolhido
         else:
             print("Opção inválida, por favor escolha um número entre 1 e 5.")
 
-def escolher_ferramenta_batalha(classe):
-    if classe == "1":
+def escolher_ferramenta_batalha(personagem):
+    if personagem["nome"] == "Paladino":
         return "lança e escudo"
-    elif classe == "2":
+    elif personagem["nome"] == "Atirador":
         return "arma"
-    elif classe == "3":
+    elif personagem["nome"] == "Guerreiro":
         return "Espada e escudo"
-    elif classe == "4":
+    elif personagem["nome"] == "Bárbaro":
         return "machado e marreta"
-    elif classe == "5":
+    elif personagem["nome"] == "Arqueiro":
         return "Arco"
 
 def escolher_montaria():
     montarias = [
-        ("Cavalo de Guerra", "3.0 m/s", "5 minutos"),
-        ("Panda", "2.5 m/s", "6 minutos"),
-        ("Lobo Gigante", "3.2 m/s", "4 minutos"),
-        ("Dragão Pequeno", "4.5 m/s", "8 minutos"),
-        ("Hipogrifo", "3.8 m/s", "7 minutos")
+        {"nome": "Leão", "velocidade": "2.0 m/s", "tempo_descanso": "3 minutos"},
+        {"nome": "Tigre", "velocidade": "1.6 m/s", "tempo_descanso": "4 minutos"},
+        {"nome": "Lobo", "velocidade": "1.8 m/s", "tempo_descanso": "4 minutos"},
+        {"nome": "Rinoceronte", "velocidade": "4.5 m/s", "tempo_descanso": "1 minutos"},
+        {"nome": "Helefante", "velocidade": "4.0 m/s", "tempo_descanso": "2 minutos"},
     ]
 
     print("\n----- Escolha sua Montaria -----")
-    for i, (nome, _, _) in enumerate(montarias, 1):
-        print(f"{i}) {nome}")
+    for i, montaria in enumerate(montarias, 1):
+        print(f"[{i}] {montaria['nome']}")
 
     while True:
         ride_choice = input("Escolha o número da sua montaria: ")
@@ -157,64 +179,18 @@ def escolher_montaria():
         if ride_choice.isdigit() and 1 <= int(ride_choice) <= len(montarias):
             index = int(ride_choice) - 1
             montaria_escolhida = montarias[index]
-            print(f"Você escolheu a montaria: {montaria_escolhida[0]}")
+            print(f"Você escolheu a montaria: {montaria_escolhida['nome']}")
             return montaria_escolhida
         else:
             print("Opção inválida, por favor escolha um número entre 1 e 5.")
 
 def customizar_avatar(avatar):
-    print("\n----- Customização do Avatar -----")
     cabelo = input("Digite o estilo de cabelo: ")
     pele = input("Digite o tom de pele: ")
-    altura = int(input("Digite a altura (em cm): "))
-    forca = int(input("Digite o nível de força (em escala de 1 a 10): "))
+    altura = input("Digite a altura (em cm): ")
+    forca = input("Digite o nível de força (em escala de 1 a 10): ")
 
     avatar.customizar_avatar(cabelo, pele, altura, forca)
-    print("Avatar customizado com sucesso!")
+    print("\nAvatar customizado com sucesso!")
 
-def distribuir_pontos_classe(classe):
-    if classe == "1":
-        atributos = ["Vida", "Mana", "Velocidade de Ataque"]
-        pontos = 100
-
-        vida = int(input("Vida: "))
-        pontos -= vida
-        mana = int(input("Mana: "))
-        pontos -= mana
-        velocidade_ataque = float(input("Velocidade de Ataque: "))
-        pontos -= velocidade_ataque
-
-        mostrar_atributos(atributos, pontos)
-
-def distribuir_pontos_montaria(montaria):
-    atributos = ["Velocidade", "Tempo para descanso"]
-    pontos = 50
-
-    velocidade = float(input("Velocidade da montaria: "))
-    pontos -= velocidade
-    tempo_descanso = int(input("Tempo para descanso da montaria: "))
-    pontos -= tempo_descanso
-
-    mostrar_atributos(atributos, pontos)
-
-def mostrar_atributos(atributos, pontos):
-    print("Distribua seus pontos:")
-    for i, atributo in enumerate(atributos):
-        print(f"{i + 1}) {atributo}: ")
-        valor = int(input())
-        pontos -= valor
-
-    print(f"Pontos restantes: {pontos}")
-
-def mostrar_informacoes(avatar, classe, ferramenta_batalha, montaria):
-    print("\n** Informações do seu personagem **")
-    print(f"Nome: {avatar.nome}")
-    print(f"Email: {avatar.email}")
-    print(f"Classe: {classe}")
-    print(f"Ferramenta de batalha: {ferramenta_batalha}")
-    print(f"Montaria: {montaria[0]}")
-    print(f"Velocidade da montaria: {montaria[1]}")
-    print(f"Tempo para descanso da montaria: {montaria[2]}")
-
-# Exemplo de uso
 tela_cadastro()
